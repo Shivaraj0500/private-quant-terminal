d64fc15f4b4530cb6f7a15bd4bddb30ece1c46c5
Shivaraj0500 <shivarajkatagaon8888@gmail.com>
2026-08-21 11:27:43 +0000

    Clean project artifacts and prepare data layer

--- src/private_quant_terminal/data/providers/__init__.py
+++ src/private_quant_terminal/data/providers/__init__.py
@@ -2,6 +2,6 @@ from private_quant_terminal.data.providers.base import MarketDataProvider
 from private_quant_terminal.data.providers.memory import InMemoryMarketDataProvider
 
 __all__ = [
-    "MarketDataProvider",
     "InMemoryMarketDataProvider",
+    "MarketDataProvider",
 ]
